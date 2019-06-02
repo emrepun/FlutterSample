f9a6d73ffcd69a2fe774112ca93abc47f43ecd24
emrepun <emrehavan@hotmail.com>
2019-06-02 12:50:17 +0100

    List rows embedded in a container.

--- lib/main.dart
+++ lib/main.dart
@@ -25,28 +25,24 @@ class HomeWidget extends StatelessWidget {
 
     return new ListView.builder(
       itemBuilder: (context, rowNumber) {
-        return new Column(
-          children: <Widget>[
-            new Image.network("https://avatars3.githubusercontent.com/u/30630955?s=400&v=4"),
-            new Text("Emrepun profile photo  $rowNumber",
-              style: new TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 18.0
-              ),),
-            new Divider(
-              color: Colors.green,
-            ) //gives divider line
-          ],
+        return new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(
+            children: <Widget>[
+              new Image.network("https://avatars3.githubusercontent.com/u/30630955?s=400&v=4"),
+              new Text("Emrepun profile photo  $rowNumber",
+                style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18.0
+                ),),
+              new Divider(
+                color: Colors.green,
+              ) //gives divider line
+            ],
+          ),
         );
       },
       itemCount: 5,
     );
-
-
-
-//    return new Center(
-//      child: new Text("heey",
-//        style: new TextStyle(fontSize: 18.0),),
-//    );
   }
 }
